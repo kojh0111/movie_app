b96fbde05b0f4a17a39194ba34e56298aaf83c1e
고정현 <kohj.h.@KOHs-MacBookPro.local>
2020-07-26 21:33:43 +0900

    Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
-import Potato from "./Potato";
+
+function Food({ favourite }) {
+  return <h1>I like {favourite}</h1>;
+}
 
 function App() {
-  return <Potato />;
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Food favourite="kimchi" />
+      <Food favourite="ramen" />
+      <Food favourite="samgiopsal" />
+      <Food favourite="chukumi" />
+    </div>
+  );
 }
 
 export default App;
