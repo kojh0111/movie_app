59202b3cdea249ae2153e83fc129a48abc9c06de
고정현 <kohj.h.@KOHs-MacBookPro.local>
2020-07-27 23:11:40 +0900

    Building the Navigation

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
+import Navigation from "./components/Navigation";
+import "./App.css";
 
 function App() {
   return (
     <HashRouter>
+      <Navigation />
       {/* path="/" exact={true}가 의미하는 바는 "/"일 때만 렌더하겠음 */}
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
