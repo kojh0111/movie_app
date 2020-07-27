2a23d01edebf2a23b2dd89e207bc4327261b91ae
고정현 <kohj.h.@KOHs-MacBookPro.local>
2020-07-27 22:53:48 +0900

    Building the Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
 
 function App() {
-  return <span>sadkf</span>;
+  return (
+    <HashRouter>
+      {/* path="/" exact={true}가 의미하는 바는 "/"일 때만 렌더하겠음 */}
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+    </HashRouter>
+  );
 }
 
 export default App;
